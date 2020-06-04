91ad15379b625cc45731d2fda0c32f3ea0c14033
jn-html <marmainjean@gmail.com>
2020-06-04 17:24:51 +0200

    Todo Item Component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -8,20 +8,19 @@ export default class TodoList extends Component {
         return (
             <ul className="list-group my-5">
                 <h3 className="text-capitalize text-center">todo list</h3>
-                {items.map(item => {return (<TodoItem 
-                key={item.id}
-                title={item.title} 
-                handleDelete={()=>handleDelete(item.id)} 
-                handleEdit={()=>handleEdit(item.id)} 
+                {items.map(item => {
+                    return (
+                        <TodoItem 
+                            key={item.id}
+                            title={item.title} 
+                            handleDelete={()=>handleDelete(item.id)} 
+                            handleEdit={()=>handleEdit(item.id)} 
 
-            />
-            );
-            })}
-            <button type="button" className="btn btn-danger btn-block text-uppercase mt-5"
-            onClick={clearList}
-            >
-                clear list
-            </button>
+                        />
+                    );
+                })}
+                <button type="button" className="btn btn-danger btn-block text-uppercase mt-5"
+                onClick={clearList}>clear list</button>
             </ul>
         );
     }
