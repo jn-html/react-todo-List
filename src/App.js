d9a14811b7d9132583fef656154e57626bb89d3c
jn-html <marmainjean@gmail.com>
2020-06-04 17:18:32 +0200

    Todo Item Component

--- src/App.js
+++ src/App.js
@@ -32,16 +32,23 @@ class App extends Component {
       id: uuid(),
       editItem: false
     }, 
-    () => console.log(this.state)
+    // () => console.log(this.state)
     );
   };
   clearList = () => {
-    console.log('clear list')
+    // console.log('clear list');
+    this.setState({
+      items: []
+    });
   }
   handleDelete = (id) => {
-    console.log(`handle edit ${id}`);
+    // console.log(`handle edit ${id}`);
+    const filteredItems = this.state.items.filter(item => item.id !== id);
+    this.setState({
+      items: filteredItems
+    });
   }
-  handleEdit = (id) => {
+  handleEdit = id => {
     console.log(`handle edit ${id}`);
   }
   render() {
